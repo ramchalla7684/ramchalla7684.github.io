8bd359fe0337d2ca2e4cf0bfd6f22a4cb91e16fd
ram <ramchalla7684@gmail.com>
2018-08-04 00:08:37 +0530

    Added mouse-scroll indicator

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,1 +1,28 @@
+let projects = [
+    {
+        "title": "Project 1",
+        "description": "Lorem ipsum dolor sit amet, dolorum fabellas intellegebat mei ex, ad consulatu argumentum eum. Eros graeco percipitur duo in. Debet putant nec ne. Vel salutatus appellantur ut. Qui ex sumo homero sensibus, quis quot usu at. In ius possim epicurei, per an ludus volutpat assueverit, maiorum constituam ne est. Eos an fugit homero. Usu ei quis reprehendunt, propriae sententiae ad vix. Mentitum accusata ut per. Populo postea nec an, duo ad illud indoctum.",
+        "preview": ""
+    },
+    {
+        "title": "Project 2",
+        "description": "Lorem ipsum dolor sit amet, dolorum fabellas intellegebat mei ex, ad consulatu argumentum eum. Eros graeco percipitur duo in. Debet putant nec ne. Vel salutatus appellantur ut. Qui ex sumo homero sensibus, quis quot usu at. In ius possim epicurei, per an ludus volutpat assueverit, maiorum constituam ne est. Eos an fugit homero. Usu ei quis reprehendunt, propriae sententiae ad vix. Mentitum accusata ut per. Populo postea nec an, duo ad illud indoctum.",
+        "preview": ""
+    },
+    {
+        "title": "Project 3",
+        "description": "Lorem ipsum dolor sit amet, dolorum fabellas intellegebat mei ex, ad consulatu argumentum eum. Eros graeco percipitur duo in. Debet putant nec ne. Vel salutatus appellantur ut. Qui ex sumo homero sensibus, quis quot usu at. In ius possim epicurei, per an ludus volutpat assueverit, maiorum constituam ne est. Eos an fugit homero. Usu ei quis reprehendunt, propriae sententiae ad vix. Mentitum accusata ut per. Populo postea nec an, duo ad illud indoctum.",
+        "preview": ""
+    }
+];
+
+
+
+
 let projectDetails = document.getElementById('project-details');
+
+
+window.scroll({
+    top: 7200,
+    behavior: 'smooth'
+});
